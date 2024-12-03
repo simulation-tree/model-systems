5a3909be6d865b0622b0092b34b19dd600e39bd6
Phill <philldamaskin@gmail.com>
2024-12-03 13:17:08 -0500

    simply system implementation

--- source/Systems/ModelImportSystem.cs
+++ source/Systems/ModelImportSystem.cs
@@ -5,17 +5,15 @@ using Meshes.Components;
 using Models.Components;
 using OpenAssetImporter;
 using Simulation;
-using Simulation.Functions;
 using System;
 using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.InteropServices;
 using Unmanaged;
 using Worlds;
 
 namespace Models.Systems
 {
-    public readonly struct ModelImportSystem : ISystem
+    public readonly partial struct ModelImportSystem : ISystem
     {
         private readonly ComponentQuery<IsModelRequest> modelRequestQuery;
         private readonly ComponentQuery<IsMeshRequest> meshRequestQuery;
@@ -24,29 +22,20 @@ namespace Models.Systems
         private readonly Dictionary<Entity, uint> meshVersions;
         private readonly List<Operation> operations;
 
-        readonly unsafe StartSystem ISystem.Start => new(&Start);
-        readonly unsafe UpdateSystem ISystem.Update => new(&Update);
-        readonly unsafe FinishSystem ISystem.Finish => new(&Finish);
-
-        [UnmanagedCallersOnly]
-        private static void Start(SystemContainer container, World world)
+        void ISystem.Start(in SystemContainer systemContainer, in World world)
         {
         }
 
-        [UnmanagedCallersOnly]
-        private static void Update(SystemContainer container, World world, TimeSpan delta)
+        void ISystem.Update(in SystemContainer systemContainer, in World world, in TimeSpan delta)
         {
-            ref ModelImportSystem system = ref container.Read<ModelImportSystem>();
-            system.Update(world);
+            Update(world);
         }
 
-        [UnmanagedCallersOnly]
-        private static void Finish(SystemContainer container, World world)
+        void ISystem.Finish(in SystemContainer systemContainer, in World world)
         {
-            if (container.World == world)
+            if (systemContainer.World == world)
             {
-                ref ModelImportSystem system = ref container.Read<ModelImportSystem>();
-                system.CleanUp();
+                CleanUp();
             }
         }
 
